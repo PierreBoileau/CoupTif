1fe20ce0829b341dc8cf50517af3079b5b79d006
olivier.moindrot <omoindrot@gmail.com>
2015-10-01 09:01:47 +0200

    Fixed number to seats

--- db/migrate/20150930173651_create_bookings.rb
+++ db/migrate/20150930173651_create_bookings.rb
@@ -2,7 +2,7 @@ class CreateBookings < ActiveRecord::Migration
   def change
     create_table :bookings do |t|
       t.string :user_name
-      t.integer :number
+      t.integer :seats
       t.references :show, index: true, foreign_key: true
 
       t.timestamps null: false
